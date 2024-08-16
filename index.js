3fcf6f55a2d34a7ab439005e664566765cbfdf05
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-08-16 17:46:08 +0400

    add: products api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8000;
 
 const app = express();
 
@@ -43,13 +43,38 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const productCollection = client.db("onlineShopDB").collection("products");
+    app.get("/products", async(req, res) => {
+        try{
+            const page = parseInt(req.query.page) || 1;
+            const limit = parseInt(req.query.limit) || 10;
+            const skip = (page - 1) * limit;
+
+            const result = await productCollection.countDocuments();
+
+            const prodcts = await productCollection.find().skip(skip).limit(limit).toArray();
+
+            res.json({
+                prodcts,
+                totalPages: Math.ceil(result/limit),
+                currentPage: page
+            });
+        }catch(error) {
+            console.log("Error fetching products", error)
+        }
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+app.get("/", (req, res) => {
+    res.send("Hello from Online Shop Server....");
+  });
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+  
