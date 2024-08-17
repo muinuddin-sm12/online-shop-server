e71f742f0597388850e2c776d3a7b0c7bed2fd8f
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-08-17 14:29:26 +0400

    modify index.js file

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const port = process.env.PORT || 8000;
 
 const app = express();
-
 const corsOptions = {
   origin: [
     "http://localhost:5173",
@@ -20,7 +19,6 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ff1pkvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,11 +26,8 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
     const productCollection = client.db("onlineShopDB").collection("products");
 
     app.get("/products", async (req, res) => {
@@ -61,13 +56,10 @@ async function run() {
         console.log("Error fetching products", error);
       }
     });
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
